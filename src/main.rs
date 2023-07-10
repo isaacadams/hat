3af7e0a19bfd2df202e8a6e36293b4e350176b82
Isaac Adams <isaacdadams@pm.me>
2023-07-10 11:24:21 -0400

    `cargo audit` found vulnerability in `chrono` (#5)
    * `cargo audit` found vulnerability in `chrono`
    
    * removing obsolete `init_logging` method
    
    * security patch

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::process::ExitCode;
 
 fn main() -> anyhow::Result<ExitCode> {
-    //init_logging();
     env_logger::init();
 
     hat::start().map(|s| {
@@ -11,30 +10,4 @@ fn main() -> anyhow::Result<ExitCode> {
             ExitCode::FAILURE
         }
     })
-}
-
-pub fn init_logging() {
-    use chrono::{DateTime, Utc};
-    use simplelog::{
-        ColorChoice, CombinedLogger, ConfigBuilder, TermLogger, TerminalMode, WriteLogger,
-    };
-
-    let datetime: DateTime<Utc> = chrono::offset::Utc::now();
-
-    CombinedLogger::init(vec![
-        TermLogger::new(
-            log::LevelFilter::Error,
-            ConfigBuilder::default().build(),
-            TerminalMode::Mixed,
-            ColorChoice::Auto,
-        ),
-        WriteLogger::new(
-            log::LevelFilter::Debug,
-            ConfigBuilder::default()
-                //.add_filter_ignore_str("yup_oauth2")
-                .build(),
-            std::fs::File::create(format!("{}.log", datetime.format("%Y-%m-%dT%H"))).unwrap(),
-        ),
-    ])
-    .unwrap();
 }
